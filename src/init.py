d23d274020fbf8d770a2f195dbc6f4fc30c35448
sharath <sharathgopinathrao@gmail.com>
2021-08-11 23:52:41 +1000

    Analyze video with Rekognition

--- src/init.py
+++ src/init.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import json
 
 logger = logging.getLogger()
 
@@ -8,8 +9,12 @@ def lambda_handler(event, context):
 
     logger.info(f"Received {len(event['Records'])} messages.")
     for record in event['Records']:
-        payload = record["body"]
-        logger.info(str(payload))
+        body = json.loads(record['body'])
+        print(body)
+
+        bucket_name = body['Records'][0]['s3']['bucket']['name']
+        object_key = body['Records'][0]['s3']['object']['key']
+        logger.info(f"FilePath: {bucket_name}/{object_key}")
 
 def setLogger():
     stdout_handler = logging.StreamHandler(sys.stdout)
