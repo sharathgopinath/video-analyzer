16d4797736e5069856f59bda8d62a4c20878f4a7
sharath <sharathgopinathrao@gmail.com>
2021-08-14 14:14:17 +1000

    Do label detection

--- src/init.py
+++ src/init.py
@@ -1,6 +1,8 @@
 import logging
 import sys
 import json
+from pprint import pprint
+import boto3
 from rekognition_video import(RekognitionVideo)
 
 logger = logging.getLogger()
@@ -16,6 +18,15 @@ def lambda_handler(event, context):
         bucket_name = body['Records'][0]['s3']['bucket']['name']
         object_key = body['Records'][0]['s3']['object']['key']
         print(f"FilePath: {bucket_name}/{object_key}")
+
+        # Analyze video
+        rekognition_client = boto3.client("rekognition")
+        video = RekognitionVideo.from_bucket(bucket_name, object_key, rekognition_client)
+        print("Detecting labels in the video.")
+        labels = video.do_label_detection()
+        print(f"Detected {len(labels)} labels, here are the first twenty:")
+        for label in labels[:20]:
+            pprint(label.to_dict())
 
 def setLogger():
     stdout_handler = logging.StreamHandler(sys.stdout)
